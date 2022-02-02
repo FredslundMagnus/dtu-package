5612bf2dc1d8d768dbc7ea93d00ac279070a47af
Magnus Fredslund <fredslundmagnus@gmail.com>
2022-02-02 11:02:17 +0100

    fix

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m"fix"
 git push
-C:/Users/magnu/Anaconda3/python.exe -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/python-helpers.git
+C:/Users/magnu/Anaconda3/python.exe -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
