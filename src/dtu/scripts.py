be573a14b195199186ee7228750dd55aee74c60f
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 18:48:21 +0100

    fix

--- src/dtu/scripts.py
+++ src/dtu/scripts.py
@@ -96,8 +96,20 @@ def cli():
 def run():
     run_clean("git pull")
     with open("experiments.sh", 'r') as file:
-        print("First", file.readline())
-        print("Second", file.readline())
+        file.readline()
+        __secret__: str = ""
+        try:
+            with open("__secret__.pyc", 'r') as secret:
+                __secret__ = secret.read()
+        except IOError:
+            pass
+        with open("__secret__.pyc", 'w') as secret:
+            temp = file.readline()
+            if __secret__ == temp:
+                answer = input("Are you sure you want to run the same experiments again? (y/n) ")
+                if answer != "y" and answer != "Y" and answer != "yes" and answer != "Yes" and answer != "YES":
+                    return
+            secret.write(temp)
         for line in file:
             run_clean(line)
     remove("submit_gpu.sh")
