9e57551dd82b82e3a81bd5161e6df97e88c02818
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 15:24:08 +0100

    fix

--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,5 @@
 git add .
 git commit -m"fix"
 git push
-python --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
+python -m pip --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
 read
