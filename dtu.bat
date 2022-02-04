90946f0bcc58e80421f4994f6e000ebf4719a230
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 15:56:22 +0100

    fix

--- dtu.bat
+++ dtu.bat
@@ -2,7 +2,7 @@ IF %1 == upgrade (
     git add .
     git commit -m"fix"
     git push
-    C:/Users/magnu/AppData/Local/Microsoft/WindowsApps/python.exe -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
+    python -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
 ) ELSE (
     echo dog
 )
