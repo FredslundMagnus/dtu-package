d659fc25d4c3727e22c2098c53fc61c3f8a6d787
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 14:34:04 +0100

    fix

--- u.bat
+++ u.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m"fix"
 git push
-python -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
+C:/Users/magnu/AppData/Local/Microsoft/WindowsApps/python.exe -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
