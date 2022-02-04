a9a81e31ec58eb8e081cdd5a73205901c240c422
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 16:24:31 +0100

    fix

--- scripts/dtu.bat
+++ scripts/dtu.bat
@@ -4,9 +4,8 @@ IF "%1" == "upgrade" (
         git add .
         git commit -m"fix"
         git push
-        python -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
     )
-    
+    python -m pip install --upgrade --force-reinstall git+https://github.com/FredslundMagnus/dtu-package.git
 ) ELSE (
     dtu_python %1
 )
