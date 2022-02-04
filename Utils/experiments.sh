7ce0db61c7a28756c5daefddc35766b4e144b96b
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 14:33:20 +0100

    fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#0225892237
+#4030106945
 mkdir ../outputs/Test1/
 mkdir ../outputs/Test1/Markdown
 bsub -o "../outputs/Test1/Markdown/Test1_0.md" -J "Test1_0" -env MYARGS="-name Test1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
