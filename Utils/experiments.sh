5968d753c669f022ce310a17026c69a4927c2ea4
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 14:10:10 +0100

    Fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#7557798583
+#3296766719
 mkdir ../outputs/Test1/
 mkdir ../outputs/Test1/Markdown
 bsub -o "../outputs/Test1/Markdown/Test1_0.md" -J "Test1_0" -env MYARGS="-name Test1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
 mkdir ../outputs/Test2/
 mkdir ../outputs/Test2/Markdown
-bsub -o "../outputs/Test2/Markdown/Test2_0.md" -J "Test2_0" -env MYARGS="-name Test2-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
+bsub -o "../outputs/Test2/Markdown/Test2_0.md" -J "Test2_0" -env MYARGS="-name Test2-0 -GPU True -time 3600 -b 4.0 -a 1 -d dssf -ID 0" < submit_gpu.sh
--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
 from main import Defaults
 
 Defaults("Test1", b=4, d="dsf")
-Defaults("Test2", b=4, d="dsf")
+Defaults("Test2", b=4, d="dssf", GPU=True)
