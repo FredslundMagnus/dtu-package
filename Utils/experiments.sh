5d265599bb47e661839794da492f50ee57d7c442
FredslundMagnus <s183905@student.dtu.dk>
2022-02-04 15:23:26 +0100

    fix

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#4030106945
+#8997996878
 mkdir ../outputs/Test1/
 mkdir ../outputs/Test1/Markdown
 bsub -o "../outputs/Test1/Markdown/Test1_0.md" -J "Test1_0" -env MYARGS="-name Test1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/python
 git add .
 git commit -m"fix"
 git push
