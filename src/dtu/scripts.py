ddb67a86785b73f93e480fadf1775c0a524d4156
FredslundMagnus <s183905@student.dtu.dk>
2022-02-05 22:00:57 +0100

    fix

--- src/dtu/scripts.py
+++ src/dtu/scripts.py
@@ -222,7 +222,15 @@ Defaults.start()
 __pycache__
 """)
     with open('experiments.sh', 'w') as f:
-        f.write("")
+        f.write("""#!/bin/sh
+mkdir -p outputs/Example1/Markdown
+bsub -o "outputs/Example1/Markdown/Example1_0.md" -J "Example1_0" -env MYARGS="-name Example1-0 -GPU False -time 3600 -b 4.0 -a 1 -d dsf -ID 0" < submit_cpu.sh
+mkdir -p outputs/Example2/Markdown
+bsub -o "outputs/Example2/Markdown/Example2_0.md" -J "Example2_0" -env MYARGS="-name Example2-0 -GPU True -time 3600 -b 4.0 -a 1 -d dssf -ID 0" < submit_gpu.sh
+mkdir -p outputs/Example3/Markdown
+bsub -o "outputs/Example3/Markdown/Example3_0.md" -J "Example3_0" -env MYARGS="-name Example3-0 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 0" < submit_cpu.sh
+bsub -o "outputs/Example3/Markdown/Example3_1.md" -J "Example3_1" -env MYARGS="-name Example3-1 -GPU False -time 3600 -b 2.0 -a 2 -d fd -ID 1" < submit_cpu.sh
+""")
 
 # def upgrade():
 #     install("git+https://github.com/FredslundMagnus/dtu-package.git")
