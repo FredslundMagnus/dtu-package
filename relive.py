422a92c8b05615f6307eac1df23a722a1fe51e7e
FredslundMagnus <s183905@student.dtu.dk>
2022-02-10 22:58:04 +0100

    fix

--- relive.py
+++ relive.py
@@ -1,34 +1,4 @@
-import inspect
-import importlib
-
-
-def _get_transfer_format(module, class_name, args, kwargs, symbol="~"):
-    return (module.__name__ + symbol + class_name + symbol + str(args) + symbol + str(kwargs)).replace(" ", "¨")
-
-
-class Parameter(type):
-    def __new__(cls, name, bases, dct):
-        x = super().__new__(cls, name, bases, dct)
-
-        old_init = x.__init__
-
-        def init(self, *args, **kwargs):
-            self._get_transfer_format = _get_transfer_format(
-                inspect.getmodule(self),
-                self.__class__.__name__,
-                args,
-                kwargs
-            )
-            old_init(self, *args, **kwargs)
-        x.__init__ = init
-        return x
-
-
-def relive(_code: str) -> Parameter:
-    module_name, class_name,  args, kwargs = _code.replace("¨", " ").split("~")
-    module = importlib.import_module(module_name)
-    _class = module.__getattribute__(class_name)
-    return _class.__call__(*eval(args), **eval(kwargs))
+from dtu import Parameter
 
 
 class P1(metaclass=Parameter):
