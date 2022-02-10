2d11b56e16d7a7b1bc09a3caf5bce8237b6bd5e9
FredslundMagnus <s183905@student.dtu.dk>
2022-02-10 23:45:11 +0100

    fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dtu import Parameters, dtu
-from relive import P1, P2
+from relive import Param
 
 
 @dtu
@@ -13,11 +13,11 @@ class Defaults(Parameters):
     b: float = 2.0
     a: int = 1
     d: str = "fd"
-    k: P1 = P1("sdf sdf", s=78)
+    k: Param = Param("sdf sdf", s=78)
 
-    def run(self, d: str, b: float, isServer: bool, k: P1) -> None:
+    def run(self, d: str, b: float, isServer: bool, k: Param) -> None:
         print(b, d, self.time, isServer)
-        print(k.name)
+        print(k.name, k.a, k.s)
 
 
 Defaults.start()
--- relive.py
+++ relive.py
@@ -1,27 +1,9 @@
 from dtu import Parameter
 
 
-class P1(metaclass=Parameter):
+class Param(metaclass=Parameter):
     a: int = 2
 
     def __init__(self, name, s=4) -> None:
         self.name = name
         self.s = s
-
-
-class P2():
-    a: int = 2
-
-    def __init__(self, name, s=4) -> None:
-        self.name = name
-        self.s = s
-
-
-# obj = P1("gs s", s=7)
-# _code = obj._get_transfer_format
-# print(P1("gs s", s=7))
-# obj2 = relive(_code)
-# print(obj.__dict__, obj2.__dict__)
-# Mistery = P2()
-# print(Bruce)
-# print(Mistery)
