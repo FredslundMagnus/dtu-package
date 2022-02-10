f303a5d4ad6a58c274578d924391798643dc659d
FredslundMagnus <s183905@student.dtu.dk>
2022-02-10 23:46:12 +0100

    fix

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 from main import Defaults
-from relive import P1
+from relive import Param
 
 Defaults("Example1", b=4, d="dsf")
 Defaults("Example2", b=4, d="dssf", GPU=True)
-Defaults("Example3", a=2, instances=2, k=P1("name2", s=11))
+Defaults("Example3", a=2, instances=2, k=Param("name2", s=11))
