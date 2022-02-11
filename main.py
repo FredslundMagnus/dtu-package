56760e00041e640f9fd45b5c71ba2540008febfe
FredslundMagnus <s183905@student.dtu.dk>
2022-02-11 21:56:28 +0100

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from __future__ import annotations
+from __future__ import annotations
 from dtu import Parameter, Parameters, dtu
 
 
@@ -22,7 +22,7 @@ class Defaults(Parameters):
 
     def run(self, d: str, b: float, isServer: bool, k: Param) -> None:
         print(b, d, self.time, isServer)
-        print(k)
+        print(k.name, k.s)
 
 
 Defaults.start()
